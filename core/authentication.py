493dc3b7ea46730e580da788a1978af326eb25d4
Angeldahal <angeldahal202@gmail.com>
2023-06-12 18:06:02 +0545

    Add password reset feature

--- core/authentication.py
+++ core/authentication.py
@@ -14,7 +14,8 @@ class JWTAuthentication(BaseAuthentication):
         if auth:
             token = auth[1]
             user_id = decode_access_token(token)
-
+            if user_id == 'Signature expired. Please log in again.':
+                raise exceptions.AuthenticationFailed("Signature expired. Please log in again.")
             user = User.objects.filter(id=user_id).first()
             return user, None
 
@@ -33,13 +34,21 @@ def decode_access_token(token):
     try:
         payload = jwt.decode(token, 'access_secret', algorithms=['HS256'])
         return payload['user_id']
-    except jwt.ExpiredSignatureError:
+    except:
         return 'Signature expired. Please log in again.'
 
 
 def create_refresh_token(id):
     return jwt.encode({
         'user_id': id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
         'iat': datetime.datetime.utcnow()
     }, 'refresh_secret', algorithm='HS256')
+
+
+def decode_refresh_token(token):
+    try:
+        payload = jwt.decode(token, 'refresh_secret', algorithms=['HS256'])
+        return payload['user_id']
+    except:
+        return 'Signature expired. Please log in again.'
--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
+import datetime
+import string
+import random
+
+from django.core.mail import send_mail
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import exceptions
 
-from .authentication import create_access_token, create_refresh_token, JWTAuthentication
-from .models import User
+from .authentication import create_access_token, create_refresh_token, JWTAuthentication, decode_refresh_token
+from .models import User, UserToken, Reset
 from .serializers import UserSerializer
 
 
@@ -38,8 +43,13 @@ class LoginAPIView(APIView):
         access_token = create_access_token(user.id)
         refresh_token = create_refresh_token(user.id)
 
+        UserToken.objects.create(
+            user_id=user.id,
+            token=refresh_token,
+            expired_at=datetime.datetime.utcnow() + datetime.timedelta(days=7),
+        )
         response = Response()
-        response.set_cookie(key=refresh_token, value=refresh_token, httponly=True)
+        response.set_cookie(key="refresh_token", value=refresh_token, httponly=True)
         response.data = {
             "token": access_token
         }
@@ -53,3 +63,77 @@ class UserAPIView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class RefreshAPIView(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        id = decode_refresh_token(refresh_token)
+
+        if not UserToken.objects.filter(
+                user_id=id,
+                token=refresh_token,
+                expired_at__gt=datetime.datetime.utcnow()
+        ).exists():
+            raise exceptions.AuthenticationFailed("User not found!")
+
+        if id == 'Signature expired. Please log in again.':
+            raise exceptions.AuthenticationFailed(id)
+        access_token = create_access_token(id)
+        return Response({
+            "token": access_token
+        })
+
+
+class LogoutAPIView(APIView):
+
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        UserToken.objects.filter(token=refresh_token).delete()
+        response = Response()
+        response.delete_cookie(key='refresh_token')
+        response.data = {
+            "message": "success"
+        }
+        return response
+
+
+class ForgotAPIView(APIView):
+    def post(self, request):
+        token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+        email = request.data.get('email', None)
+        Reset.objects.create(
+            email=email,
+            token=token,
+        )
+        url = 'http://localhost:3000/reset/' + token
+        send_mail(
+            subject='Reset Password',
+            message='click {url} to reset your password '.format(url=url),
+            from_email="angeldahal@email.com",
+            recipient_list=[email],
+        )
+
+        return Response({
+            "message": "success"
+        })
+
+
+class ResetAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        if data['password'] != data['confirm_password']:
+            raise exceptions.APIException("Passwords do not match!")
+        token = data['token']
+        reset = Reset.objects.filter(token=token).first()
+        if reset is None:
+            raise exceptions.APIException("Invalid token!")
+        user = User.objects.filter(email=reset.email).first()
+        if not user:
+            raise exceptions.APIException("User not found!")
+        user.set_password(data['password'])
+        user.save()
+        return Response({
+            "message": "success"
+        })
